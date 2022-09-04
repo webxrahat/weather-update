00e34fec3bc70cb5eda5ecf1ab00d623b10676f6
islammd39e <islammdrahat308@gmail.comm>
2022-09-04 20:04:23 +0600

    upload on file

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}'
 const urlApi = 'ac8a5795573febd427041bd612b4ee2b'
 
-const country_api = async(city) =>{
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${urlApi}`
+const country_api = async (city) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${urlApi}&units=metric`
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  tptr(data);
 }
-country_api()
+
+const tptr = data => {
+  const weather = document.getElementById('temperature')
+  weather.innerText = data.main.temp;
+}
+
+document.getElementById('btn-search').addEventListener('click', function () {
+  const field = document.getElementById('input-field');
+  const city = field.value;
+  document.getElementById('city').innerText = city;
+  country_api(city)
+
+})
+
+
+country_api('dhaka')
